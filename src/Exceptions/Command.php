3d533166a814709c6921a4a591fdf66a45f8d153
mishu <mishu@xtelecom.ro>
2020-01-08 09:41:28 +0200

    bump

--- src/Exceptions/Command.php
+++ src/Exceptions/Command.php
@@ -8,13 +8,11 @@ class Command extends Exception
 {
     public static function notFound($result)
     {
-        return new self('Executable Not Found'.PHP_EOL
-            .implode(PHP_EOL, $result));
+        return new self('Executable Not Found'.PHP_EOL.implode(PHP_EOL, $result));
     }
 
     public static function executionFailed($result)
     {
-        return new self('Cannot scan the document'.PHP_EOL
-            .implode(PHP_EOL, $result));
+        return new self('Cannot scan the document'.PHP_EOL.implode(PHP_EOL, $result));
     }
 }
